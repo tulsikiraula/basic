25df95f79eaeefa85c0f500895b9aad4eb62a2da
tulsi kiraula <tulsi.kiraula111@gmail.com>
2020-08-11 19:15:37 +0530

    first absic commit

---
+++ src/main/java/basic/MathCalculation.java
@@ -0,0 +1,13 @@
+package basic;
+
+public class MathCalculation {
+
+	public static void main(String[] args) {
+		MathCalculation obj = new MathCalculation();
+		obj.add(1, 2);
+	}
+	private void add(int a, int b) {
+		System.out.println("sum of two values :"+  a+b);
+	}
+
+}
